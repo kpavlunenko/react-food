421fca0ba13cbe4d4b5a93e73fd51f0fe2a16a85
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2023-11-29 18:16:32 +0200

    react-food

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {Recipe} from "./pages/Recipe";
 
 function App() {
     return (<>
-        <BrowserRouter>
+        <BrowserRouter basename='/react-food'>
             <Header/>
             <main className='container content'>
                 <Routes>
